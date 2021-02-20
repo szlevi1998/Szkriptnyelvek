d85c44e899cfec3971006986dc2f947db508ee98
szlevi1998 <sz.levi1998@gmail.com>
2021-02-20 16:00:12 +0100

    first commit

---
+++ Óraimunka/formazas.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env/ python3
+
+def hello(name,  color, what):
+    #geza, kek az eg!
+    #C-ben:
+    # print f("%s, %s az %s!\n", name, color, what);
+    # v1
+    print(" 1) {0}, {1} a {2}!".format(name.capitalize(),color,what))
+    # v2
+    #print(" 2) {}, {} az {}!".format(name, color, what))
+    # v3
+    #print(" 3) {nev}, {szin}, az {mi}!".format(nev = name, szin = color, mi = what))
+    # v4
+    #print(f"{name}, {color} az {what}!")
+
+def main():
+    hello("geza", "kek", "eg")
+    print("-"* 40)
+    hello("steph","Golden","Warriors")
+##############################################
+
+if __name__ == "__main__":
+    main()
