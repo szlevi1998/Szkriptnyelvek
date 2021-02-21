dfbf37fa75c2d85feed957546c86e60192211a7c
szlevi1998 <sz.levi1998@gmail.com>
2021-02-21 15:16:45 +0100

    Összeadásos feladat megoldva

---
+++ Házifeladatok/osszeadas.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/ python3
+
+def main():
+    print("Add meg az első számot!")
+    elso = input()
+    print("Add meg az második számot!")
+    masodik = input()
+    osszeg = int(elso) + int(masodik)
+    print("A két szám összege:", osszeg )
+
+
+if __name__ == '__main__':
+    main()
--- Óraimunka/formazas.py
+++ Óraimunka/formazas.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env/ python3
 
-def hello(name,  color, what):
-    #geza, kek az eg!
-    #C-ben:
+def hello(name, color, what):
+    # geza, kek az eg!
+    # C-ben:
     # print f("%s, %s az %s!\n", name, color, what);
     # v1
-    print(" 1) {0}, a {1} a {2} franchise történetének legjobb játékosa!".format(name.capitalize(),color,what))
+    print(" 1) {0}, a {1} a {2} franchise történetének legjobb játékosa!".format(name.capitalize(), color, what))
     # v2
-    #print(" 2) {}, {} az {}!".format(name, color, what))
+    # print(" 2) {}, {} az {}!".format(name, color, what))
     # v3
-    #print(" 3) {nev}, {szin}, az {mi}!".format(nev = name, szin = color, mi = what))
+    # print(" 3) {nev}, {szin}, az {mi}!".format(nev = name, szin = color, mi = what))
     # v4
-    #print(f"{name}, {color} az {what}!")
+    # print(f"{name}, {color} az {what}!")
+
 
 def main():
     hello("geza", "kek", "eg")
-    print("-"* 40)
-    hello("steph","Golden","Warriors")
+    print("-" * 40)
+    hello("steph", "Golden", "Warriors")
+
+
 ##############################################
 
 if __name__ == "__main__":
