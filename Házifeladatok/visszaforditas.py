40bbbfa01fd818e9c3c7490c34d636d08eaab143
szlevi1998 <sz.levi1998@gmail.com>
2021-02-21 17:03:24 +0100

    Számjegyes visszafordítós feladat kész

---
+++ Házifeladatok/visszaforditas.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env/ python3
+
+
+def main():
+    print("Adj meg egy számot!")
+    szam = input()
+    forditott = str(szam)[::-1]
+
+    if forditott[0] == "0":
+        print("A szám utolsó számjegye 0 ezért ezt nem lehet megfordítani")
+    else:
+        print(forditott)
+
+
+if __name__ == '__main__':
+    main()
