a11868d83d32f2e60181edf53a250887f13c9157
szlevi1998 <sz.levi1998@gmail.com>
2021-03-03 17:49:53 +0100

    Stringtisztítós feladat kész.

---
+++ Házifeladatok/Házifeladatok2/stringclean.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def clean(address):
+    hugo = address.replace("\n", '')
+
+    if "\n" in address:
+        print("The new IP: ", hugo)
+    else:
+        print("The same IP: ", address)
+
+
+def main():
+    clean("192.20.246.138: 6666")
+
+
+if __name__ == '__main__':
+    main()
