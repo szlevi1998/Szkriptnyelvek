5133e34d0621e7cf9723e310600454fb8d68e734
szlevi1998 <sz.levi1998@gmail.com>
2021-03-10 14:37:36 +0100

    Sortöréses feladat kész.

--- Házifeladatok/Házifeladatok3/sortores.py
+++ Házifeladatok/Házifeladatok3/sortores.py
@@ -1,13 +1,19 @@
-import sys
+#!/usr/bin/env python3
+
 import random as r
 
 UPTO = 100
 
 
 def main():
+    first = 0
+
     for i in range(UPTO):
         print(r.randint(0, 9), end="")
-    print(UPTO)
+        first += 1
+        if first == 10:
+            print("\t")
+            first -= 10
 
 
 if __name__ == '__main__':
